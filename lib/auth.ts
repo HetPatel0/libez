6e7ebd08f4eb7dc527e6e0a3612f2dec9dfa11bc
HetPatel0 <142023310+HetPatel0@users.noreply.github.com>
2025-08-10 11:15:08 +0530

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,20 +1,88 @@
 // lib/auth.ts
 import type { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
+import prisma from "./prisma"
+import { compare } from "bcryptjs"
 
 export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: 'jwt'
+  },
   providers: [
     CredentialsProvider({
-      name: "Credentials",
+      name: 'Sign in',
       credentials: {
-        username: {},
-        password: {}
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'hello@example.com'
+        },
+        password: { label: 'Password', type: 'password' }
       },
       async authorize(credentials) {
-        // Your logic here
-        return null
+        if (!credentials?.email || !credentials.password) {
+          return null
+        }
+
+        const user = await prisma.users.findUnique({
+          where: {
+            UserEmail: credentials.email
+          }
+        })
+
+        if (!user) {
+          return null
+        }
+
+        const isPasswordValid = await compare(
+          credentials.password,
+          user.UserPass
+        )
+
+        if (!isPasswordValid) {
+          return null
+        }
+
+        return {
+          id: user.UserID + '',
+          email: user.UserEmail,
+          name: user.UserName,
+          randomKey: 'Hey cool'
+        }
       }
     })
   ],
-  session: { strategy: "jwt" }
+  callbacks: {
+    session: ({ session, token }) => {
+      console.log('Session Callback', { session, token })
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+          randomKey: token.randomKey
+        }
+      }
+    },
+    jwt: ({ token, user }) => {
+      console.log('JWT Callback', { token, user })
+      if (user) {
+        const u = user as unknown as any
+        return {
+          ...token,
+          id: u.id,
+          randomKey: u.randomKey
+        }
+      }
+      return token
+    }
+  },
+
+  pages :{
+     signIn: "/auth/signin",     
+     signOut:"/" ,
+    newUser: "/auth/register",      // Custom register page (optional)
+    error: "/auth/error", 
+  }
 }
+
