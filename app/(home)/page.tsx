942363f7ed90b75e2c9090aab63107f03a5e9fb4
HetPatel0 <142023310+HetPatel0@users.noreply.github.com>
2025-08-10 10:50:36 +0530

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,15 +1,11 @@
 import Herosec from "@/components/herosec";
 
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
-import { LoginButton, LogoutButton } from "@/components/auth";
-
 export default async function Home() {
   // const session = await getServerSession(authOptions);
   return (
     <>
       {/* <pre> {JSON.stringify(session)} </pre> */}
-      
+
       <Herosec />
     </>
   );
